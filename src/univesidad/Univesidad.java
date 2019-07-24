945819a633e046f732b2f0d7652ac0b702d5463e
Luis Gutierrez <alejandroluis444@gmail.com>
2019-07-24 15:45:39 -0500

    Modificando

--- src/univesidad/Univesidad.java
+++ src/univesidad/Univesidad.java
@@ -1,46 +1,153 @@
 
 package univesidad;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Univesidad {
 
-    
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);
-        Alumnos alumno1,alumno2 ;
+        Alumnos alumno1;
         Administrativo adm1;
         catedratico cate1;
         Planta planta1;
         
-        alumno1=new Alumnos("Ti", "1001346644", "gutierrez pimienta", "Luis alejanadro",17,
-        true, "2000114343654", "Ingenieria", "sistema", 7);
         
-        adm1=new Administrativo("Cc", "1001346644", "gutierrez pimienta", "Luis alejanadro",40,
-        true, "jefe", "20-06-2016");
+        String tipoDocumento , Documento, Apellido,  Nombres, cargo , fechaContrato,
+                codigo, facultad,  programa, profesion, asignatura;
         
-        cate1= new catedratico("Cc", "1001346644", "gutierrez pimienta", "Luis alejanadro",40,
-        true, "Ing.sistema ", "Calculo diferencial","20-06-2016", "25-09-2018");
+        int edad,semestre;
+        boolean activo;
         
-        planta1= new Planta("Cc", "1001346644", "gutierrez pimienta", "Luis alejanadro",40,
-        true, "Ing.sistema ", "Calculo diferencial","20-06-2016",20);
         
-        System.out.println("Datos del Alumno");
-        System.out.println("");
-        System.out.println(alumno1.mostrar());
-        System.out.println("");
-        System.out.println("Datos del administrador");
-        System.out.println("");
-        System.out.println(adm1.mostrar());
-        System.out.println("");
-        System.out.println("Datos del catedratico");
-        System.out.println("");
-        System.out.println(cate1.mostrar());
-        System.out.println("");
-        System.out.println("Datos de planta");
-        System.out.println("");
-        System.out.println(planta1.mostrar());
+        int op=0;
+        
+        ArrayList <Alumnos> PersonalAlumno = new ArrayList();
+        ArrayList <Administrativo> Administrativo = new ArrayList();
+       boolean seguir=true;
+       
+        do{
+            
+            System.out.println("-------Registro Universidad-----");
+            System.out.println("--------------------------------");
+            System.out.println("[1] Alunmos");
+            System.out.println("[2] Adiministrativo");
+            System.out.println("[3] Docentes");
+            System.out.println("[4] Mostrar Datos");
+            System.out.println("Seleccione una opcion");
+            System.out.println("---------------------------------");
+            op=leer.nextInt();
+        
+            switch(op){
+                case 1:
+                    System.out.println("Ingrese Tipo de Documento: ");
+                    tipoDocumento=leer.next();
+                    System.out.println("");
+
+                    System.out.println("Ingrese Documento: ");
+                    Documento=leer.next();
+                    System.out.println("");
+
+                    System.out.println("Ingreses Nombres: ");
+                    Nombres=leer.next();
+                    System.out.println("");
+
+                    System.out.println("Ingrese Apellidos: ");
+                    Apellido=leer.next();
+                    System.out.println("");
+
+                    System.out.println("Ingrese edad: ");
+                    edad=leer.nextInt();
+                    System.out.println("");
+
+                    System.out.println("Ingrese Estado true / false: ");
+                    activo=leer.nextBoolean();
+                    System.out.println("");
+
+                    System.out.println("Ingrese Codigo: ");
+                    codigo=leer.next();
+                    System.out.println("");
+
+                    System.out.println("Ingrese Facultadad:");
+                    facultad=leer.next();
+                    System.out.println("");
+
+                    System.out.println("Ingrese Programa: ");
+                    programa=leer.next();
+                    System.out.println("");
+
+                    System.out.println("Ingrese Semestre: ");
+                    semestre=leer.nextInt();
+                    System.out.println("");
+
+
+
+                    alumno1=new Alumnos(tipoDocumento, Documento, Apellido, Nombres,edad,
+                    activo, codigo, facultad,programa,semestre);
+
+                    PersonalAlumno.add(alumno1);             
+                break;
+                
+                case 2:
+                    
+                    System.out.println("Ingrese Tipo de Documento: ");
+                    tipoDocumento=leer.next();
+                    System.out.println("");
+
+                    System.out.println("Ingrese Documento: ");
+                    Documento=leer.next();
+                    System.out.println("");
+
+                    System.out.println("Ingreses Nombres: ");
+                    Nombres=leer.next();
+                    System.out.println("");
+
+                    System.out.println("Ingrese Apellidos: ");
+                    Apellido=leer.next();
+                    System.out.println("");
+
+                    System.out.println("Ingrese edad: ");
+                    edad=leer.nextInt();
+                    System.out.println("");
+
+                    System.out.println("Ingrese Estado true / false: ");
+                    activo=leer.nextBoolean();
+                    System.out.println("");
+                    
+                    System.out.println("Ingrese cargo: ");
+                    cargo=leer.next();
+                    System.out.println("");
+                    
+                    System.out.println("Ingrese fecha de contrato: ");
+                    fechaContrato=leer.next();
+                    System.out.println("");
+
+                    adm1= new Administrativo(tipoDocumento, Documento, Apellido, Nombres,edad,
+                        activo,cargo,fechaContrato );
+                    
+                    Administrativo.add(adm1);
+                    break;
+                    
+                    
+                case 3:
+                    break;
+            }
+        }while(seguir=!false);
+        
+             
+       
+                
+                
+         
+        
+        
+        
+        
+        
         
         
     }
+    
+        
     
 }
